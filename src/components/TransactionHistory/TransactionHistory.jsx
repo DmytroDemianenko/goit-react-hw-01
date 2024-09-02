f7811652fc1c886cf8f4f5ec28ccc32b26d94ad1
dmytro_demianenko <demyanenkodmitriy@gmail.com>
2024-09-02 22:11:33 +0200

    style for table

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -14,7 +14,7 @@ export default function TransactionHistory({ items }) {
         {items.map((item) => {
           return (
             <tr key={item.id}>
-              <td>{item.type}</td>
+              <td className={s.rowItem}>{item.type}</td>
               <td>{item.amount}</td>
               <td>{item.currency}</td>
             </tr>
